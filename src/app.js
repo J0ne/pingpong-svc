21597c437f0c932b26986f70852f705b30aa088f
Jouni Männistö <jouni.mannisto@gmail.com>
2021-11-14 13:01:26 +0200

    pingpong route

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require("express");
 
 const app = express();
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-const port = 3001;
+let requestCounter = 0;
+const port = 3003;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+app.get("/pingpong", (req, res) => {
+  res.send(`pong ${requestCounter}`);
+  requestCounter++;
+});
 
 app.listen(port, () => {
+
   console.log(`Server started in port ${port}`);
 });
