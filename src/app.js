11fc18f8e9ed0fb00380004e4d7e8d8ac8058dd7
Jouni Männistö <jouni.mannisto@gmail.com>
2021-11-17 11:52:29 +0200

    Exercise 1.11: Persisting data

--- src/app.js
+++ src/app.js
@@ -1,15 +1,33 @@
 const express = require("express");
+const path = require("path");
+const fsPromises = require("fs").promises;
 
+const directory = path.join("/", "usr", "src", "files");
+const filePath = path.join(directory, "pong.txt");
 const app = express();
 // app.use(express.static("public"));
 
 let requestCounter = 0;
+let state = '';
 const port = 3003;
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
+
+const writeToFile = async (msg) => {
+  try {
+    await fsPromises.writeFile(filePath, msg);
+    console.log(`${msg} > pong.txt`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 app.get("/pingpong", (req, res) => {
-  res.send(`pong ${requestCounter}`);
+  state = `Ping / pongs: ${requestCounter}`
+  writeToFile(state);
+  res.send(state);
+
   requestCounter++;
 });
 
